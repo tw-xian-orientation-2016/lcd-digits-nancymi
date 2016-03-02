936f6161b4fee12e3856f6fae5e3df29caf3a2aa
nancymi <13121178nancy@gmail.com>
2016-03-02 15:57:11 +0800

    Add: test translate

--- test/translate-test.js
+++ test/translate-test.js
@@ -1,3 +1,13 @@
 /**
  * Created by nancymi on 16/3/2.
  */
+describe('translate(): traslate a number list to a number lcd list', function() {
+   it('when give a number:int list then print a numberLCD:object list', function() {
+       var input = [9, 1, 0];
+       var output = [new numberLCD('._.', '|_|', '..|'),
+           new numberLCD('...', '..|', '..|'),
+           new numberLCD('._.', '|.|', '|_|')];
+
+       expect(translate(input)).toEqual(output);
+   }) ;
+});
