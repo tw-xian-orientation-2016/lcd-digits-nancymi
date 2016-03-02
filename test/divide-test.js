08f3488f5ec936dd7acd985f1125afaadee963b3
nancymi <13121178nancy@gmail.com>
2016-03-02 15:56:27 +0800

    Add: test divide

---
+++ test/divide-test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nancymi on 16/3/2.
+ */
+describe('divide(): divide a number to numberlist', function() {
+   it('given a number:string then print a numberlist:int[]', function() {
+       expect(divide('910')).toEqual([9, 1, 0]);
+   }) ;
+});
